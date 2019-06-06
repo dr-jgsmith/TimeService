7cb1a84cc350d3e5165cb988ba7bb4e16338596f
Justin Smith <jgriffissmith@j-griffis-smiths-imac.local>
2019-06-05 20:44:35 -0700

    update tests

--- tests/test_timeservice.py
+++ tests/test_timeservice.py
@@ -10,9 +10,10 @@ def test_setting_time():
 def test_setting_time_and_future_time():
     time_obj = TimeMachine('9:15 AM', 10)
     assert time_obj.value == 10
-    assert time_obj.time() == '9:15 AM'
+    assert time_obj.get_time() == '9:15 AM'
 
 
 def test_add_time():
     new_time = TimeMachine('9:15 AM', 10).add_minutes()
     assert new_time == '9:25 AM'
+
